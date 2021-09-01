92b22fee87ce3286bfee159702f6c1dec13bdcdc
DaniboyXX <89485436+DaniboyXX@users.noreply.github.com>
2021-09-01 14:09:53 -0600

    Add files via upload

---
+++ 9_1_21_Daniel_chavez_hello_world.py
@@ -0,0 +1,76 @@
+#Daniel Chavez
+#Hello World Program
+#9-2021
+
+
+
+#import this
+
+
+##################
+#Main
+##################
+#help("keywords")
+
+#intro
+print("enter your info\n\n\n")
+
+first_name=input("enter first name")
+print(first_name)
+
+last_name=input("enter last name")
+print(last_name)
+
+social_security_number=input("enter social security number")
+print(social_security_number)
+
+maidens_name=input("enter maidens name")
+print(maidens_name)
+
+date_of_birth=input("date of birth")
+print(date_of_birth)
+
+Credit_card_number=input("Credit card number")
+print(Credit_card_number)
+
+cvv_number=input("cvv number")
+print(cvv_number)
+
+expiration_date=input("expiration date")
+print(expiration_date)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# keep window open
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
